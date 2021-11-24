d2377aeda61be7f69f4bf35b11a179bed20e19c6
0sama-rao <94981090+0sama-rao@users.noreply.github.com>
2021-11-24 22:57:23 +0500

    Main Page of newsfeed created
    Frontend of Main page created with containing:
    - Accordion
    - Newsfeed
    - Footer
    - Modal 
    - NewsfeedItems and Newsfeed lists

---
+++ src/Business/NewsFeed/Data.js
@@ -0,0 +1,14 @@
+export const Data = [
+  {
+    question: 'Pakistan to pursue preferential trade deals with Saudi Arabia, UAE, Oman: Razak Dawood',
+    answer: 'A labracadabrador.'
+  },
+  {
+    question: 'Demand for used cars flourishing amid costly new ones?',
+    answer: 'Hill-arious.'
+  },
+  {
+    question: 'PSO chief eyes 50pc market share in petrol, diesel soon?',
+    answer: 'I Apollo-gize.'
+  }
+];
