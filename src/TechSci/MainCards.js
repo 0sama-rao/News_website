e86f18a0aaa7a87ca39459ed99a801cac84cb5fe
0sama-rao <94981090+0sama-rao@users.noreply.github.com>
2021-11-24 22:36:47 +0500

    Technology and scince frontend created

---
+++ src/TechSci/MainCards.js
@@ -0,0 +1,166 @@
+import React from 'react';
+
+import './MainCards.css';
+
+const MainCards = ()=> {
+  return (
+    <body>
+
+<div class="main">
+
+
+
+<div class="card">
+
+<div class="image">
+   <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Gfp-missouri-st-louis-clubhouse-pond-and-scenery.jpg/1199px-Gfp-missouri-st-louis-clubhouse-pond-and-scenery.jpg"/>
+</div>
+<div class="title">
+ <h1>
+Write title Here</h1>
+</div>
+<div class="des">
+ <p>You can Add Desccription Here...</p>
+<button>Read More...</button>
+</div>
+</div>
+
+
+
+<div class="card">
+
+<div class="image">
+   <img src="https://cdn.pixabay.com/photo/2018/01/09/03/49/the-natural-scenery-3070808_1280.jpg"/>
+</div>
+<div class="title">
+ <h1>
+Write title Here</h1>
+</div>
+<div class="des">
+ <p>You can Add Desccription Here...</p>
+<button>Read More...</button>
+</div>
+</div>
+
+
+
+<div class="card">
+
+<div class="image">
+   <img src="https://cdn.pixabay.com/photo/2015/11/07/11/41/lake-1031405_1280.jpg"/>
+</div>
+<div class="title">
+ <h1>
+Write title Here</h1>
+</div>
+<div class="des">
+ <p>You can Add Desccription Here...</p>
+<button>Read More...</button>
+</div>
+</div>
+
+
+
+<div class="card">
+
+<div class="image">
+   <img src="https://cdn.pixabay.com/photo/2018/01/09/03/49/the-natural-scenery-3070808_1280.jpg"/>
+</div>
+<div class="title">
+ <h1>
+Write title Here</h1>
+</div>
+<div class="des">
+ <p>You can Add Desccription Here...</p>
+<button>Read More...</button>
+</div>
+</div>
+
+
+
+<div class="card">
+
+<div class="image">
+   <img src="https://cdn.pixabay.com/photo/2018/01/09/03/49/the-natural-scenery-3070808_1280.jpg"/>
+</div>
+<div class="title">
+ <h1>
+Write title Here</h1>
+</div>
+<div class="des">
+ <p>You can Add Desccription Here...</p>
+<button>Read More...</button>
+</div>
+</div>
+
+<div class="card">
+
+<div class="image">
+   <img src="https://cdn.pixabay.com/photo/2018/01/09/03/49/the-natural-scenery-3070808_1280.jpg"/>
+</div>
+<div class="title">
+ <h1>
+Write title Here</h1>
+</div>
+<div class="des">
+ <p>You can Add Desccription Here...</p>
+<button>Read More...</button>
+</div>
+</div>
+
+
+<div class="card">
+
+<div class="image">
+   <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Gfp-missouri-st-louis-clubhouse-pond-and-scenery.jpg/1199px-Gfp-missouri-st-louis-clubhouse-pond-and-scenery.jpg"/>
+</div>
+<div class="title">
+ <h1>
+Write title Here</h1>
+</div>
+<div class="des">
+ <p>You can Add Desccription Here...</p>
+<button>Read More...</button>
+
+</div>
+</div>
+
+
+
+<div class="card">
+
+<div class="image">
+   <img src="https://cdn.pixabay.com/photo/2018/01/09/03/49/the-natural-scenery-3070808_1280.jpg"/>
+</div>
+<div class="title">
+ <h1>
+Write title Here</h1>
+</div>
+<div class="des">
+ <p>You can Add Desccription Here...</p>
+<button>Read More...</button>
+</div>
+</div>
+
+
+
+<div class="card">
+
+<div class="image">
+   <img src="https://cdn.pixabay.com/photo/2015/11/07/11/41/lake-1031405_1280.jpg"/>
+</div>
+<div class="title">
+ <h1>
+Write title Here</h1>
+</div>
+<div class="des">
+ <p>You can Add Desccription Here...</p>
+<button>Read More...</button>
+</div>
+</div>
+</div>
+</body>
+  );
+
+};
+export default MainCards;
